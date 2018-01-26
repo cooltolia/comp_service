5a8e044b5b8405752488ee21717754e5d63f2f19
kulak <cooltolia@gmail.com>
2018-01-26 13:49:06 +0300

    еще пара блоков

--- app/js/main.js
+++ app/js/main.js
@@ -251,6 +251,58 @@
     
 
     
+    (function() {
+
+    
+
+        var $spots = $('.spots'),
+
+            $car = $('.spots__car');
+
+    
+
+        var spotsHeight = $spots.outerHeight(),
+
+            spotsOffset = $spots.offset(),
+
+            spotsTopPosition = spotsOffset.top - spotsHeight;
+
+    
+
+        $(window).scroll(function () {
+
+            
+
+            var wScroll = $(this).scrollTop();
+
+    
+
+            if (wScroll >= spotsTopPosition) {
+
+                var dynamic = wScroll - spotsTopPosition;
+
+                
+
+                $car.css({
+
+                    'background-position': dynamic / 5 + '% top'
+
+                })
+
+            }
+
+    
+
+        })
+
+    
+
+    })();
+
+    
+    
+
+    
     
 
     
